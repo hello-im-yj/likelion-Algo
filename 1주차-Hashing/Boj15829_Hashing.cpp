050b60da757fa12d05c1fe5278ec836fa85f80ee
YujinKim-Sciedu <sandwich17yj@likelion.org>
2020-09-22 17:06:23 +0900

    Update Boj15829_Hashing.cpp

---
+++ 1주차-Hashing/Boj15829_Hashing.cpp
@@ -0,0 +1,58 @@
+#include <iostream>		//c++ 표준입출력 함수 사용
+#include <string>
+
+//cin = console input (>>(오른쪽 꺽쇠)는 Shift를 누른 채로 .(점)을 누르면 됩니다.)
+//cout = console output (<< (왼쪽 꺽쇠)는 Shift를 누른 채로, (쉼표)를 누르면 됩니다.)
+
+#define M 1234567891
+#define r 31
+
+using namespace std;
+using ll = long long;
+
+ll mod(int num, int count);
+
+int main() {
+	int length;
+	cin >> length;
+
+	string input;
+	cin >> input;
+
+	ll hash = 0;
+
+	for (int i = 0; i < length; i++) {
+		hash = (hash + mod(input[i]-'a' + 1, i)) % M;
+	}
+
+	cout << hash;
+}
+
+ll mod(int num, int count) {
+	ll res = num;
+	for (int i = 0; i < count; i++) {
+		res = (res * r) % M;
+	}
+
+	return res;
+}
+
+/*
+제산 함수 기본형
+
+int hash_function(int key) {
+	int hash_index = key % M;
+	if (hash_index < 0) {
+		hash_index += M;
+	}
+	return hash_index;
+}
+*/
+
+/*
+모듈러 연산 기본 속성
+1. (a + b) mod n = {(a mod n) + (b mod n)} mod n
+2. (a - b) mod n = {(a mod n) - (b mod n)} mod n
+3. (a * b) mod n = {(a mod n) * (b mod n)} mod n
+
+*/
